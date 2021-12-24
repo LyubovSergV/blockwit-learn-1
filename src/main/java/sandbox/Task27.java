281c50c3ce1692ccee28c570c9e8654ea1a75bfc
LyubovSergV <lyubovsergeeva95@gmail.com>
2021-12-24 16:45:35 +0300

    Task27 add

---
+++ src/main/java/sandbox/Task27.java
@@ -0,0 +1,13 @@
+//Есть массив данных:
+//9,8,7,6,5,4,3,2,1
+//Вывести числа в обратном порядке
+package sandbox;
+
+public class Task27 {
+    public static void main(String[] args) {
+        int[] Nambers = {9, 8, 7, 6, 5, 4, 3, 2, 1};
+        for (int i = 8; i >= 0; i--){
+            System.out.println(Nambers[i]);
+        }
+    }
+}
