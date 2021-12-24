bd0a8a9dfcfd9324467a457e0838af2964e6ea4d
LyubovSergV <lyubovsergeeva95@gmail.com>
2021-12-24 16:46:56 +0300

    Task27 kjkf

--- src/main/java/sandbox/Task27.java
+++ src/main/java/sandbox/Task27.java
@@ -7,7 +7,7 @@ public class Task27 {
     public static void main(String[] args) {
         int[] Nambers = {9, 8, 7, 6, 5, 4, 3, 2, 1};
         for (int i = 8; i >= 0; i--){
-            System.out.println(Nambers[i]);
+            System.out.println(" "+Nambers[i]);
         }
     }
 }
