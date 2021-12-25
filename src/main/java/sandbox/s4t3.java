9d9b46537c68da008546c853c9aa3435b71a9e1c
LyubovSergV <lyubovsergeeva95@gmail.com>
2021-12-25 16:51:09 +0300

    задача 3

--- src/main/java/sandbox/s4t3.java
+++ src/main/java/sandbox/s4t3.java
@@ -7,37 +7,22 @@ import java.util.Scanner;
 public class s4t3 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        String[] names = new String[4];
+        int[] yourOfBirth = new int[4];
+        int[] age = new int[4];
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Введите имя:");
+            names[i] = in.nextLine();
+        }
+        for (int i = 0; i < 4; i++) {
+            System.out.println("Введите год рождения");
+            yourOfBirth[i] = in.nextInt();
+            age[i] = 2021 - yourOfBirth[i];
+        }
 
-        System.out.println("Введите первое имя");
-        String nameFirst = in.nextLine();
-        System.out.println("Введите год рождения");
-        int yearOfBirthFirst = in.nextInt();
-
-        System.out.println("Введите второе имя");
-        String nameSecond = in.nextLine();
-        System.out.println("Введите год рождения");
-        int yearOfBirthSecond = in.nextInt();
-
-        System.out.println("Введите третье имя");
-        String nameThird = in.nextLine();
-        System.out.println("Введите год рождения");
-        int yearOfBirthThird = in.nextInt();
-
-        System.out.println("Введите четвертое имя");
-        String nameFourth = in.nextLine();
-        System.out.println("Введите год рождения");
-        int yearOfBirthFourth = in.nextInt();
-
-        int ageFirst = 2021 - yearOfBirthFirst;
-        int ageSecond = 2021 - yearOfBirthSecond;
-        int ageThird = 2021 - yearOfBirthThird;
-        int ageFourth = 2021 - yearOfBirthFourth;
-
-
-        System.out.println(nameFirst + "! Вам " + ageFirst + " лет");
-        System.out.println(nameSecond + "! Вам " + ageSecond + " лет");
-        System.out.println(nameThird + "! Вам " + ageThird + " лет");
-        System.out.println(nameFourth + "! Вам " + ageFourth + " лет");
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i] + " вам " + age[i]);
+        }
 
         in.close();
     }
