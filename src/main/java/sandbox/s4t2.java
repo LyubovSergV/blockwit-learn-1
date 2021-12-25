b98b293cfa58b99d01b7bf3746e24ecb580d2938
LyubovSergV <lyubovsergeeva95@gmail.com>
2021-12-25 16:29:22 +0300

    задача 2

--- src/main/java/sandbox/s4t2.java
+++ src/main/java/sandbox/s4t2.java
@@ -7,18 +7,17 @@ import java.util.Scanner;
 
 public class s4t2 {
     public static void main(String[] args) {
+
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите первое имя");
-        String nameFirst = in.nextLine();
-        System.out.println("Введите второе имя");
-        String nameSecond = in.nextLine();
-        System.out.println("Введите третье имя");
-        String nameThird = in.nextLine();
-        System.out.println("Введите четвертое имя");
-        String nameFourth = in.nextLine();
-        System.out.println("Введите пятое имя");
-        String nameFifth = in.nextLine();
-        System.out.println(nameFirst +", "+ nameSecond +", "+ nameThird +", "+ nameFourth +", "+ nameFifth);
+        String[] names = new String[5];
+        for (int i = 0; i < names.length; i++) {
+            System.out.println("Введите имя: ");
+            names[i] = in.nextLine();
+        }
+        for (int i = 0; i < names.length; i++) {
+            System.out.println(names[i]);
+        }
+
         in.close();
     }
 }
